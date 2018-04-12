3c442933c35ddfe50ee95528e0361ecfe7ab8ab1
v666 <v666@1nterc3pt0r.org>
2018-04-12 19:49:11 +0200

    Operaciones finales

--- src/m5/uf3/git/Circulo.java
+++ src/m5/uf3/git/Circulo.java
@@ -3,7 +3,8 @@ package m5.uf3.git;
 public class Circulo {
 	
 	private int radio;
-
+	private double area;
+	
 	public Circulo(int radio) {
 		super();
 		this.radio = radio;
@@ -16,4 +17,17 @@ public class Circulo {
 	public void setRadio(int radio) {
 		this.radio = radio;
 	}
+	
+	public double getArea() {
+		return area;
+	}
+
+	public void setArea(double area) {
+		this.area = area;
+	}
+	
+	public void calcularArea(){
+		setArea(Math.PI * Math.pow(radio,2));
+	}
+
 }
